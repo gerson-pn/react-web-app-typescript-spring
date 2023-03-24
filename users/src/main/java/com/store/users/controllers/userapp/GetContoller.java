c17789229b3c9f9628e726e266415ccf00c650b7
Gerson Penha <gerson.penha@fatec.sp.gov.br>
2023-03-24 07:45:59 -0300

    Inclusão da annotation @CrossOrigin e do início da aplicação front-end

--- users/src/main/java/com/store/users/controllers/userapp/GetContoller.java
+++ users/src/main/java/com/store/users/controllers/userapp/GetContoller.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.store.users.entities.UserApp;
 import com.store.users.services.userapp.UserAppReader;
 
+@CrossOrigin
 @RestController
 public class GetContoller {
 	@Autowired
--- users/src/main/java/com/store/users/controllers/userapp/PutController.java
+++ users/src/main/java/com/store/users/controllers/userapp/PutController.java
@@ -2,6 +2,7 @@ package com.store.users.controllers.userapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.store.users.entities.UserApp;
 import com.store.users.services.userapp.UserAppUpdater;
 
+@CrossOrigin
 @RestController
 public class PutController {
 
